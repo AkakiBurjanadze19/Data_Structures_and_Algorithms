af06d579263a6271845bdb8aeca19d3e0d46f528
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-22 19:05:19 +0400

    test linear search algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import Algorithms.BinarySearch;
+import Algorithms.LinearSearch;
 
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
 
-        System.out.println(BinarySearch.binarySearch(arr, 5));
-        System.out.println(BinarySearch.binarySearch(arr, 10));
+//        System.out.println(BinarySearch.binarySearch(arr, 5));
+//        System.out.println(BinarySearch.binarySearch(arr, 10));
+
+        System.out.println(LinearSearch.linearSearch(arr, 5));
+        System.out.println(LinearSearch.linearSearch(arr, 10));
     }
 }
