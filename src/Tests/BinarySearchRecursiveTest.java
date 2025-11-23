7fd8515ab47ff0b0a50c9f6bd1774baf2ebaceb1
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-23 18:56:07 +0400

    add BinarySearchRecursiveTest.java

--- src/Tests/BinarySearchRecursiveTest.java
+++ src/Tests/BinarySearchRecursiveTest.java
@@ -1,4 +1,15 @@
 package Tests;
+import Algorithms.Searching.BinarySearchRecursive;
 
 public class BinarySearchRecursiveTest {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int result = BinarySearchRecursive.binarySearch(arr, 5, 0, arr.length - 1);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
 }
