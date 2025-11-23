35ba81ede0238af967cc8c2f44108d5ccbc9292f
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-23 15:20:18 +0400

    add BinarySearchTest.java

--- src/Tests/BinarySearchTest.java
+++ src/Tests/BinarySearchTest.java
@@ -1,4 +1,15 @@
 package Tests;
+import Algorithms.Searching.BinarySearch;
 
 public class BinarySearchTest {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int result = BinarySearch.binarySearch(arr, 5);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
 }
