0c3fb1194c5166a741f4262106643296d1acd68a
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-23 21:08:09 +0400

    MaximumSubarrayTest.java

--- src/Tests/MaximumSubarrayTest.java
+++ src/Tests/MaximumSubarrayTest.java
@@ -1,4 +1,11 @@
 package Tests;
+import java.util.Arrays;
+import Algorithms.Array.MaximumSubarray;
 
 public class MaximumSubarrayTest {
+    public static void main(String[] args) {
+        int[] arr = {10, -5, 15, -2, 20, -15, 86, 40, -5};
+
+        System.out.println(Arrays.toString(MaximumSubarray.findMaximumSubarray(arr, 0, arr.length - 1)));
+    }
 }
