6c686af970f05c9d6a88334d24cc319a4257fe26
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-23 18:55:13 +0400

    add BinarySearchRecursive.java

--- src/Algorithms/Searching/BinarySearchRecursive.java
+++ src/Algorithms/Searching/BinarySearchRecursive.java
@@ -1,4 +1,23 @@
 package Algorithms.Searching;
 
+/*
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+ */
 public class BinarySearchRecursive {
+    public static int binarySearch(int[] array, int element, int left, int right) {
+        // base case
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = left + (right - left) / 2;
+        if (array[mid] == element) {
+            return mid;
+        } else if (array[mid] < element) {
+            return binarySearch(array, element, mid + 1, right);
+        } else {
+            return binarySearch(array, element, left, mid - 1);
+        }
+    }
 }
