73caa931d1ff99751bef5e689cd1b75f1565b17f
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-23 21:07:57 +0400

    add MaximumSubarraySumTest.java

--- src/Tests/MaximumSubarraySumTest.java
+++ src/Tests/MaximumSubarraySumTest.java
@@ -1,4 +1,11 @@
 package Tests;
+import Algorithms.Array.MaximumSubarraySum;
 
 public class MaximumSubarraySumTest {
+    public static void main(String[] args) {
+        int[] arr = {10, -5, 15, -2, 20, -15, 86, 40, -5};
+
+        int maxSubArraySum = MaximumSubarraySum.findMaximumSubarraySum(arr, 0, arr.length - 1);
+        System.out.println(maxSubArraySum);
+    }
 }
