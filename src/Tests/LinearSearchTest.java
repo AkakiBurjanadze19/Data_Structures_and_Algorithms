5dc49a1a41067db40a700704f7dc7701e35c853d
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-23 15:20:04 +0400

    add LinearSearchTest.java

--- src/Tests/LinearSearchTest.java
+++ src/Tests/LinearSearchTest.java
@@ -1,4 +1,15 @@
 package Tests;
+import Algorithms.Searching.LinearSearch;
 
 public class LinearSearchTest {
+    public static void main(String[] args) {
+        int[] arr = {30, 20, 12, 15, 16};
+
+        int result = LinearSearch.linearSearch(arr, 16);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
 }
