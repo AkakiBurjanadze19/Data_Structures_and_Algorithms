82e3048c286789791539d4ec01da25adb0b0ff5a
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-23 15:20:33 +0400

    add InsertionSortTest.java

--- src/Tests/InsertionSortTest.java
+++ src/Tests/InsertionSortTest.java
@@ -1,4 +1,15 @@
 package Tests;
+import Algorithms.Sorting.InsertionSort;
+import java.util.Arrays;
 
-public class InsertionSort {
+public class InsertionSortTest {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 4, 6, 1, 3};
+
+        System.out.println("array before sorting: " + Arrays.toString(arr));
+
+        InsertionSort.insertionSort(arr);
+
+        System.out.println("array after sorting: " + Arrays.toString(arr));
+    }
 }
