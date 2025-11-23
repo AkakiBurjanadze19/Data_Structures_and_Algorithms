3fd5f679516111d23061317cda8f071e77b6f9b5
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-23 18:55:58 +0400

    add LinearSearchRecursiveTest.java

--- src/Tests/LinearSearchRecursiveTest.java
+++ src/Tests/LinearSearchRecursiveTest.java
@@ -1,4 +1,15 @@
 package Tests;
+import Algorithms.Searching.LinearSearchRecursive;
 
 public class LinearSearchRecursiveTest {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int result = LinearSearchRecursive.linearSearch(arr, 50, 0);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
 }
