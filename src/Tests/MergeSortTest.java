021db0cd008aea2e32971d798692831b2553018f
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-23 15:20:43 +0400

    add MergeSortTest.java

--- src/Tests/MergeSortTest.java
+++ src/Tests/MergeSortTest.java
@@ -1,4 +1,15 @@
 package Tests;
+import Algorithms.Sorting.MergeSort;
+import java.util.Arrays;
 
 public class MergeSortTest {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 15, 3, 30, 25, 18};
+
+        System.out.println("array before sorting: " + Arrays.toString(arr));
+
+        MergeSort.mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("array after sorting: " + Arrays.toString(arr));
+    }
 }
