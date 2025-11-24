86da9cf566c240e9aff34f9ae9a84d474e8d3da2
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-24 17:22:10 +0400

    add SquareMatrixMultiplyTest.java

--- src/Tests/SquareMatrixMultiplyTest.java
+++ src/Tests/SquareMatrixMultiplyTest.java
@@ -1,4 +1,18 @@
 package Tests;
+import utils.Matrix;
+import Algorithms.Matrix.SquareMatrixMultiply;
 
 public class SquareMatrixMultiplyTest {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+                {1, 2},
+                {4, 5}
+        };
+        int[][] matrix2 = {
+                {1, 2},
+                {4, 5}
+        };
+
+        Matrix.printMatrix(SquareMatrixMultiply.squareMatrixMultiply(matrix1, matrix2));
+    }
 }
