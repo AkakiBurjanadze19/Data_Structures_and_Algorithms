a52eb367be1d5bd33df8b7c0cbc1222a052f3272
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-24 17:22:19 +0400

    add Matrix.java

--- src/utils/Matrix.java
+++ src/utils/Matrix.java
@@ -1,4 +1,12 @@
 package utils;
 
 public class Matrix {
+    public static void printMatrix(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
