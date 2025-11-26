1e89a77b6c4271f97e75ff3f284802ecfd32fb5b
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-26 20:48:41 +0400

    change method name to sort in MergeSort2 class

--- src/Algorithms/Sorting/MergeSort2.java
+++ src/Algorithms/Sorting/MergeSort2.java
@@ -42,11 +42,11 @@ public class MergeSort2 {
         Time Complexity: O(n log n)
         Space Complexity: O(n)
      */
-    public static void mergeSort(int[] array, int left, int right) {
+    public static void sort(int[] array, int left, int right) {
         if (left < right) {
             int mid = left + (right - left) / 2;
-            mergeSort(array, left, mid);
-            mergeSort(array, mid + 1, right);
+            sort(array, left, mid);
+            sort(array, mid + 1, right);
             merge(array, left, mid, right);
         }
     }
