33adb65b88bae2be185ab507ac3dabfa1a211200
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-26 21:32:29 +0400

    add BinaryLocate.java

---
+++ src/Algorithms/Searching/BinaryLocate.java
@@ -0,0 +1,24 @@
+package Algorithms.Searching;
+
+public class BinaryLocate {
+    /*
+        Time Complexity: O(log n)
+        Space Complexity: O(1)
+     */
+    public static int locate(int[] array, int element) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] < element) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
