66f388113fa0c25fbedafbb1128d1a142dd534ad
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-27 01:08:21 +0400

    add MaxHeapTest.java

--- src/Tests/MaxHeapTest.java
+++ src/Tests/MaxHeapTest.java
@@ -1,4 +1,13 @@
 package Tests;
+import Data_Structures.MaxHeap;
+import java.util.Arrays;
 
 public class MaxHeapTest {
+    public static void main(String[] args) {
+        int[] arr = {1, 20, 30, 15, 22, 3, 35};
+
+        MaxHeap.buildHeap(arr);
+
+        System.out.println("Max Heap: " + Arrays.toString(arr));
+    }
 }
