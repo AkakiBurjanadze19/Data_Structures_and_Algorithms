46afa2229578a6487794eb0ec5362067e9564523
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-27 01:07:44 +0400

    add BubbleSortTest.java

--- src/Tests/BubbleSortTest.java
+++ src/Tests/BubbleSortTest.java
@@ -1,4 +1,16 @@
 package Tests;
+import Algorithms.Sorting.BubbleSort;
+
+import java.util.Arrays;
 
 public class BubbleSortTest {
+    public static void main(String[] args) {
+        int[] arr = {30, 15, 10, 25, 50, 12, 5, 1};
+
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+
+        BubbleSort.sort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
 }
