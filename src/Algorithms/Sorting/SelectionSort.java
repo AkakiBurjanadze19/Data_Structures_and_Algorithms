8a5a6da0747d76d405cd1c771ed089c843821668
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-27 01:06:47 +0400

    add SelectionSort.java

--- src/Algorithms/Sorting/SelectionSort.java
+++ src/Algorithms/Sorting/SelectionSort.java
@@ -1,4 +1,28 @@
 package Algorithms.Sorting;
 
 public class SelectionSort {
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    /*
+        Time Complexity: O(n^2)
+        Space Complexity: O(1)
+     */
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                swap(array, i, minIndex);
+            }
+        }
+    }
 }
