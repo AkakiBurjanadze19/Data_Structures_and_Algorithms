d9a8c40385b12262826104114df47e85bf1704b8
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-27 01:07:52 +0400

    add SelectionSortTest.java

--- src/Tests/SelectionSortTest.java
+++ src/Tests/SelectionSortTest.java
@@ -1,4 +1,17 @@
 package Tests;
+import Algorithms.Sorting.BubbleSort;
+import Algorithms.Sorting.SelectionSort;
+
+import java.util.Arrays;
 
 public class SelectionSortTest {
+    public static void main(String[] args) {
+        int[] arr = {30, 15, 10, 25, 50, 12, 5, 1};
+
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+
+        SelectionSort.sort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
 }
