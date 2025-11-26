22327b8591f466a17777a831459a5e7533d0aea6
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-27 01:08:28 +0400

    add MinHeapTest.java

--- src/Tests/MinHeapTest.java
+++ src/Tests/MinHeapTest.java
@@ -1,4 +1,14 @@
 package Tests;
 
+import Data_Structures.MinHeap;
+import java.util.Arrays;
+
 public class MinHeapTest {
+    public static void main(String[] args) {
+        int[] arr = {1, 20, 30, 15, 22, 3, 35};
+
+        MinHeap.buildHeap(arr);
+
+        System.out.println("Min Heap: " + Arrays.toString(arr));
+    }
 }
