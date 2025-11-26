c3769948eb218f62a355ddf717b3cd508223f8a3
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-27 01:08:01 +0400

    add HeapSortTest.java

--- src/Tests/HeapSortTest.java
+++ src/Tests/HeapSortTest.java
@@ -1,4 +1,15 @@
 package Tests;
+import Algorithms.Sorting.HeapSort;
+import java.util.Arrays;
 
 public class HeapSortTest {
+    public static void main(String[] args) {
+        int[] arr = {30, 15, 10, 25, 50, 12, 5, 1};
+
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+
+        HeapSort.sort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
 }
