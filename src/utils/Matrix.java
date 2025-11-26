9d85aefa3e3e0e935e5891f1847cef081ed96330
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-26 20:57:40 +0400

    change parameter type to double

--- src/utils/Matrix.java
+++ src/utils/Matrix.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class Matrix {
-    public static void printMatrix(int[][] A) {
+    public static void printMatrix(double[][] A) {
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[i].length; j++) {
                 System.out.print(A[i][j] + " ");
