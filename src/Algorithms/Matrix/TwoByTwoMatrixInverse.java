42b3ef0c0cd2561907db6f6caea7b54d3f3fecd2
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-11-26 20:56:05 +0400

    change method name to compute in TwoByTwoMatrixInverse class, also change return and parameter types to double.

--- src/Algorithms/Matrix/TwoByTwoMatrixInverse.java
+++ src/Algorithms/Matrix/TwoByTwoMatrixInverse.java
@@ -1,4 +1,23 @@
 package Algorithms.Matrix;
 
 public class TwoByTwoMatrixInverse {
+    public static double[][] compute(double[][] A) {
+        double a = A[0][0];
+        double d = A[1][1];
+        double b = A[0][1];
+        double c = A[1][0];
+
+        double det = a * d - b * c;
+
+        if (det == 0) {
+            throw new IllegalArgumentException("Matrix is singular and cannot be inverted");
+        }
+
+        double[][] inverseMatrix = {
+                {d / det, -b / det},
+                {-c / det, a / det}
+        };
+
+        return inverseMatrix;
+    }
 }
